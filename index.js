7bccfcde9e2037f76adc4fd3f06badc036fd63e4
Vanessa Schwegman <vschwegman@gmail.com>
2017-09-03 21:54:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } elseif (string === "I love you, Grandma.") {
     return "I love you, too.";
+  } else {
+    return "huh!";
   }
 }
